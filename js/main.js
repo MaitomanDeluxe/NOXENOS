d4da1f8b1e24917dda7f4a08b3b14b3d26eb7b1f
MaitomanDeluxe <maitomandeluxe@outlook.jp>
2026-07-14 10:25:32 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+import {Shell} from "./kernel/shell.js";
 
+window.addEventListener("load",()=>{
+
+setTimeout(()=>{
+
+document.getElementById("boot-screen").style.display="none";
+
+document.getElementById("desktop").style.display="block";
+
+new Shell();
+
+},1800);
+
+});
