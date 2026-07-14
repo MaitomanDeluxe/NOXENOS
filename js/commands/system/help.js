c5a986d938ac7f41c1003b8fccedb00341136f59
MaitomanDeluxe <maitomandeluxe@outlook.jp>
2026-07-14 10:32:45 +0900

    Create help.js

---
+++ js/commands/system/help.js
@@ -0,0 +1,23 @@
+export default function(shell){
+
+shell.print("");
+
+shell.print("Available commands");
+
+shell.print("");
+
+shell.print("help");
+
+shell.print("clear");
+
+shell.print("echo");
+
+shell.print("pwd");
+
+shell.print("whoami");
+
+shell.print("ls");
+
+shell.print("");
+
+}
