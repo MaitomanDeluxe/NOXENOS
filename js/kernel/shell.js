7a464ab0977cb08bc5acf8fdcc505af37ee8af91
MaitomanDeluxe <maitomandeluxe@outlook.jp>
2026-07-14 10:25:49 +0900

    Update shell.js

--- js/kernel/shell.js
+++ js/kernel/shell.js
@@ -1,1 +1,29 @@
+export class Shell{
 
+constructor(){
+
+this.print("NOXEN OS v1.0");
+
+this.print("");
+
+this.print("Type 'help' to begin.");
+
+}
+
+print(text){
+
+const terminal=document.getElementById("terminal");
+
+const line=document.createElement("div");
+
+line.className="line";
+
+line.innerHTML=text;
+
+terminal.appendChild(line);
+
+terminal.scrollTop=terminal.scrollHeight;
+
+}
+
+}
